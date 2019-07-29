86ed000c88566612f68cd8789ef7b39e7af4f295
Andressaggudelo <cagudeloa@uninorte.edu.co>
2019-07-28 21:09:12 -0500

    Two of the four lines working already. Change angle (0-90) in line 49.

--- src/points/Points.java
+++ src/points/Points.java
@@ -46,54 +46,115 @@ public class Points extends JPanel {
         g2d.drawLine(0, h / 2, w, h / 2);
         g2d.drawLine(w / 2, 0, w / 2, h);
         
-        for (int i = 15; i <= 90; i+=15) {
-            coordinates(g2d, i, w, h);
-        }
-    }
-    
-    public void coordinates(Graphics2D g2d, int angle, int w, int h){
-        g2d.setColor(Color.blue);
+        int angle = 30;
+        int ang = angle;
         Point points[] = new Point[5];
-        //g2d.setStroke(new BasicStroke(2));
         for (int i = 0; i < 4; i++) {
             int x_pos = (int)Math.round(200*Math.cos(Math.toRadians(angle)));
             int y_pos  = (int)Math.round(200*Math.sin(Math.toRadians(angle)));
-            points[i] = new Point(x_pos, y_pos);
-            g2d.drawLine(w/2+x_pos, h/2+y_pos, w/2+x_pos, h/2+y_pos);
+            points[i] = new Point(w/2+x_pos, h/2+y_pos);
             angle += 90;
         }
         points[4] = new Point(points[0].x, points[0].y);
         
-        g2d.drawLine(w/2-points[0].x, h/2-points[0].y, w/2-points[1].x, h/2-points[1].y);
-        g2d.drawLine(w/2+points[1].x, h/2+points[1].y, w/2+points[2].x, h/2+points[2].y);
-        g2d.drawLine(w/2-points[2].x, h/2-points[2].y, w/2-points[3].x, h/2-points[3].y);
-        g2d.drawLine(w/2+points[3].x, h/2+points[3].y, w/2+points[4].x, h/2+points[4].y);
-        
+        coordinates(g2d, w, h, points);
+        bresenham(g2d, points, w, h, ang);
     }
+    
+    
+    public void bresenham(Graphics g, Point points[], int w, int h, int angle){
+        g.setColor(Color.blue);
+        for (int i = 0; i < 4; i++) {
+            
+            int x1 = points[i].x;
+            int y1 = points[i].y;
+            int x2 = points[i+1].x;
+            int y2 = points[i+1].y;
+            int dx = Math.abs(x2-x1);
+            int dy = Math.abs(y2-y1);
 
-    public void bresenham(int w, int h, Graphics2D g2d, int x1, int y1, int x2, int y2, int sign) {
+            if(Math.abs((float)dy/dx)<=1 && (i == 0 || i == 2) ){ //Solo funciona para 2 de las 4 líneas
+                System.out.println("Move on 'x'");
 
-        int dx = Math.abs(x2 - x1);
-        int dy = Math.abs(y2 - y1);
-        // Transformaion
-        x1 += w / 2;
-        y1 += h / 2;
-        x2 += w / 2;
-        int incE = 2 * dy;
-        int incNE = 2 * dy - 2 * dx;
-        int d = 2 * dy - dx;
-        int y = y1;
-        for (int x = x1; x <= x2; x++) {
-            g2d.drawLine(x, y, x, y);
-            if (d <= 0) {
-                d += incE;
-            } else {
-                d += incNE;
-                y += 1 * sign;
+                int incE = 2*dy;
+                int incNE = incE - 2*dx;
+                int d = incE - dx;
+
+
+                if(x1<x2){
+                    int y = y1;
+                    if(angle<=45){
+                        System.out.println("el ciclo va de x1 a x2, increasing. 'Y' decrementa.");
+                        for (int x = x1; x <= x2; x++) {
+                            g.drawLine(x, y, x, y);
+                            if(d<=0){
+                                d += incE;
+                            }else{
+                                d += incNE;
+                                y -= 1;
+                            }
+                        }
+                    }else{
+                        System.out.println("el ciclo va de x1 a x2, increasing. 'Y' aumenta.");
+                        for (int x = x1; x <= x2; x++) {
+                            g.drawLine(x, y, x, y);
+                            if(d<=0){
+                                d += incE;
+                            }else{
+                                d += incNE;
+                                y += 1;
+                            }
+                        }
+                    }
+                }else{
+                    int y = y2;
+                    if(angle<=45){
+                        System.out.println("el ciclo va de x2 a x1, decreasing. 'Y' decrementa.");
+                        for (int x = x2; x <= x1; x++) {
+                            g.drawLine(x, y, x, y);
+                            if(d<=0){
+                                d += incE;
+                            }else{
+                                d += incNE;
+                                y -= 1;
+                            }
+                        }
+                    }else{
+                        System.out.println("el ciclo va de x2 a x1, decreasing. 'Y' incrementa.");
+                        for (int x = x2; x <= x1; x++) {
+                            g.drawLine(x, y, x, y);
+                            if(d<=0){
+                                d += incE;
+                            }else{
+                                d += incNE;
+                                y += 1;
+                            }
+                        }
+                    }
+                    // el ciclo va de x2 a x1, decreasing, Y aumenta
+                }
             }
         }
-
+        
+        
+        
+        
+        
+//        if(Math.abs((float)dy/dx)>1 /* && (i == 1 || i == 3) */){
+//            System.out.println("Move on 'y'");
+//        }
+    
     }
+    
+    
+    public void coordinates(Graphics2D g2d, int w, int h, Point points[]){
+        g2d.setColor(Color.blue);        
+        for (int i = 0; i < 1; i++) {
+            //g2d.drawLine(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+            //System.out.println("x1: "+points[i].x+"  y1: "+points[i].y+"    x2: "+points[i+1].x+"  y2: "+points[i+1].y);
+        }
+    }
+   
 
     public static void main(String[] args) {
 
